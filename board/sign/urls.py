0bd4a670cc16a08d502c2030d62c5e63561d1aae
ZuevEvgeniy <124444672+ZuevEvgeniy@users.noreply.github.com>
2023-08-23 16:40:16 +0300

    Регистрацию добавил 1.0

---
+++ board/sign/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import activate, signup
+
+urlpatterns = [
+    path('login/',
+         LoginView.as_view(template_name = 'login.html'),
+         name='login'),
+    path('logout/',
+         LogoutView.as_view(template_name = 'logout.html'),
+         name='logout'),
+    path('signup/', signup, name = 'signup'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+        activate, name='activate'),
+]
